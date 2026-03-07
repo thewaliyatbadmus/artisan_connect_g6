28c830f9c9ee293113432578f08d27c290e2d9f1
shakilla1 <s.uwamahoro1@alustudent.com>
2026-03-07 10:39:58 -0800

    update schema file

--- schema.sql
+++ schema.sql
@@ -3,3 +3,12 @@ DROP DATABASE IF EXISTS artisan_connect;
 CREATE DATABASE artisan_connect;
 USE artisan_connect;
 
+-- USERS TABLE
+CREATE TABLE users (
+    user_id INT AUTO_INCREMENT PRIMARY KEY,
+    full_name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    role ENUM('artisan','customer') NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
